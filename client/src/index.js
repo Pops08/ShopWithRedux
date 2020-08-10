7c8b791a5a7cf621f10e2bdd2620093db13a64e0
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-08-09 23:30:22 -0700

    Server updates

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import store from './store'
-import { Provider } from 'react-redux'
-
 
 ReactDOM.render(
   <React.StrictMode>
